32bb994422e0d7b0f5b749678bf5603235b8378b
Christopher Kodama <ckodama@redhat.com>
2025-01-31 16:30:28 -0500

    add more timeouts to monitor-snapshot.sh

--- tools/snapshot-generator/monitor-snapshot.sh
+++ tools/snapshot-generator/monitor-snapshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e 
+set -eo pipefail
 
 # USAGE:
 # ./monitor_snapshot.sh SNAPSHOT_NAME INTEGRATION_TEST_NAME OUTPUT_FILE_NAME
@@ -8,25 +8,18 @@ snapshot=$1
 integration_test=$2
 output_file=$3
 
-# epoch=$(date +%s)
-# 
-# snapshot="snapshot-sample-$epoch"
-# integration_test=konflux-sandbox-enterprise-contract
-# output_file=./ec-logs.txt
-# 
-# kubectl get snapshot konflux-sandbox-x8wck -o json | jq --arg name "$snapshot" '{apiVersion, kind, spec, metadata: {name: $name, namespace: .metadata.namespace, labels: {"test.appstudio.openshift.io/type": "override"} }}' | kubectl apply -f -
+echo waiting for snapshot creation...
+kubectl wait --for create snapshot "$snapshot" --timeout=10m
 
-# kubectl wait --for create snapshot "$snapshot"
-# kubectl label snapshot "$snapshot" "test.appstudio.openshift.io/run=$integration_test"
-
+echo "getting pipelinerun..."
 pipelinerun=$(kubectl get pr -l "appstudio.openshift.io/snapshot=$snapshot,test.appstudio.openshift.io/scenario=$integration_test" --no-headers | awk '{print $1}')
 
 pod_name="${pipelinerun}-verify-pod"
 
 echo "waiting for $pod_name to be created"
-kubectl wait --for=create pod "$pod_name"
+kubectl wait --for=create pod "$pod_name" --timeout=20m
 echo "waiting for $pod_name to finish"
-kubectl wait --for='jsonpath={.status.conditions[?(@.reason=="PodCompleted")].status}=True' pod "$pod_name"
+kubectl wait --for='jsonpath={.status.conditions[?(@.reason=="PodCompleted")].status}=True' pod "$pod_name" --timeout=60m
 
 
 kubectl logs "$pod_name" step-report-json > $output_file
