379ee8a9c312b7c421008c95ba820af3623ecf74
Christopher Kodama <ckodama@redhat.com>
2025-01-31 16:50:40 -0500

    update monitor-snapshot.sh
    if the pipelinerun name is too long ,the pod name gets truncated

--- tools/snapshot-generator/monitor-snapshot.sh
+++ tools/snapshot-generator/monitor-snapshot.sh
@@ -12,9 +12,12 @@ echo waiting for snapshot creation...
 kubectl wait --for create snapshot "$snapshot" --timeout=10m
 
 echo "getting pipelinerun..."
-pipelinerun=$(kubectl get pr -l "appstudio.openshift.io/snapshot=$snapshot,test.appstudio.openshift.io/scenario=$integration_test" --no-headers | awk '{print $1}')
+pipelinerun=$(kubectl get pipelinerun -l "appstudio.openshift.io/snapshot=$snapshot,test.appstudio.openshift.io/scenario=$integration_test" --no-headers | awk '{print $1}')
 
-pod_name="${pipelinerun}-verify-pod"
+echo "waiting for verify task to start..."
+kubectl wait --for='jsonpath={.status.childReferences[?(@.pipelineTaskName=="verify")]}' pipelinerun "$pipelinerun" --timeout=10m
+task_name=$(kubectl get pipelinerun rhoai-v2-17-registry-rhoai-prod-enterprise-contract-n25zc -o jsonpath='{.status.childReferences[0].name}')
+pod_name="${task_name}-pod"
 
 echo "waiting for $pod_name to be created"
 kubectl wait --for=create pod "$pod_name" --timeout=20m
