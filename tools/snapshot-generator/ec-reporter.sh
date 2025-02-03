6e575ad5973e55dba0d38a60b8be4ce79cd3774a
Christopher Kodama <ckodama@redhat.com>
2025-02-03 09:04:02 -0500

    update ec-reporter
    remove MODE as an input parameter, make it loop instead

--- tools/snapshot-generator/ec-reporter.sh
+++ tools/snapshot-generator/ec-reporter.sh
@@ -14,7 +14,6 @@ set -eo pipefail
 # KUBERNETES_SERVICE_HOST - should be set automatically by k8s
 # KUBERNETES_SERVICE_PORT_HTTPS - should be set automatically by k8s
 
-MODE=$1
 
 source ./ubi9-minimal-install.sh
 
@@ -28,51 +27,52 @@ APPLICATION=$(echo $VERSION | awk -F '.' '{print "rhoai-v" $1 "-" $2 }')
 # requires RHOAI_QUAY_API_TOKEN env var to be set
 bash ./make-nightly-snapshots.sh "$VERSION"
 
-if [ "$MODE" = fbc ]; then
-  ec_test="$APPLICATION-fbc-rhoai-prod-enterprise-contract"
-  snapshot_folder="nightly-snapshots/snapshot-fbc"
-else
-  ec_test="$APPLICATION-registry-rhoai-prod-enterprise-contract"
-  snapshot_folder="nightly-snapshots/snapshot-components"
-fi
 
+for MODE in components fbc; do
+  MESSAGE=
+  if [ "$MODE" = fbc ]; then
+    ec_test="$APPLICATION-fbc-rhoai-prod-enterprise-contract"
+    snapshot_folder="nightly-snapshots/snapshot-fbc"
+  else
+    ec_test="$APPLICATION-registry-rhoai-prod-enterprise-contract"
+    snapshot_folder="nightly-snapshots/snapshot-components"
+  fi
 
+  # apply and mark snapshot so that the EC gets run against it
+  kubectl apply -f $snapshot_folder
+  snapshot_name=$(kubectl get -f $snapshot_folder --no-headers | awk '{print $1}')
+  echo kubectl label snapshot "$snapshot_name" "test.appstudio.openshift.io/run=$ec_test"
+  kubectl label snapshot "$snapshot_name" "test.appstudio.openshift.io/run=$ec_test"
 
-# apply and mark snapshot so that the EC gets run against it
-kubectl apply -f $snapshot_folder
-snapshot_name=$(kubectl get -f $snapshot_folder --no-headers | awk '{print $1}')
-echo kubectl label snapshot "$snapshot_name" "test.appstudio.openshift.io/run=$ec_test"
-kubectl label snapshot "$snapshot_name" "test.appstudio.openshift.io/run=$ec_test"
+  # monitor pipelinerun 
+  ec_results_file=./$MODE-results.json 
+  monitor_snapshot_output=./monitor-$MODE-snapshot-output.txt
+  bash ./monitor-snapshot.sh "$snapshot_name" "$ec_test" "$monitor_snapshot_output" 
 
-# monitor pipelinerun 
-ec_results_file=./components-results.json 
-monitor_snapshot_output=./monitor-snapshot-output.txt
-bash ./monitor-snapshot.sh "$snapshot_name" "$ec_test" "$monitor_snapshot_output" 
+  echo "processing log output"
 
-echo "processing log output"
+  PIPELINE_NAME=$(cat "$monitor_snapshot_output" | tail -n 1 )
+  cat $monitor_snapshot_output | tail -n 2 | head -n 1 > "$ec_results_file"
 
-PIPELINE_NAME=$(cat "$monitor_snapshot_output" | tail -n 1 )
-cat $monitor_snapshot_output | tail -n 2 | head -n 1 > "$ec_results_file"
+  WEB_URL="https://konflux.apps.stone-prod-p02.hjvn.p1.openshiftapps.com/application-pipeline/workspaces/rhoai/applications/$APPLICATION" 
 
-WEB_URL="https://konflux.apps.stone-prod-p02.hjvn.p1.openshiftapps.com/application-pipeline/workspaces/rhoai/applications/$APPLICATION" 
+  # create formatted yaml file to send to slack
+  echo "Selecting violations out of ec results file"
+  cat "$ec_results_file" | jq '[.components[] | select(.violations)] | map({name, containerImage, violations: [.violations[] | {msg} + (.metadata | {description, solution})]}) ' | tee "./$MODE-ec-results-slack.json"
+  echo "converting to yaml"
+  cat "./$MODE-ec-results-slack.json" | yq -P | tee "./$MODE-ec-results-slack.yaml"
 
-# create formatted yaml file to send to slack
-echo "Selecting violations out of ec results file"
-cat "$ec_results_file" | jq '[.components[] | select(.violations)] | map({name, containerImage, violations: [.violations[] | {msg} + (.metadata | {description, solution})]}) ' | tee "./ec-results-slack.json"
-echo "converting to yaml"
-cat "./ec-results-slack.json" | yq -P | tee "./ec-results-slack.yaml"
+  # create inital slack message
+  echo "parsing results for slack message"
+  num_errors=$(cat "$ec_results_file"| jq '[.components[].violations | length] | add')
+  num_warnings=$(cat "$ec_results_file" | jq '[.components[].warnings | length] | add')
+  num_error_components=$(cat "$ec_results_file" | jq '[.components[] | select(.violations) | .name] | length')
+  num_warning_components=$(cat "$ec_results_file" | jq '[.components[] | select(.warnings) | .name] | length')
 
-# create inital slack message
-echo "parsing results for slack message"
-num_errors=$(cat "$ec_results_file"| jq '[.components[].violations | length] | add')
-num_warnings=$(cat "$ec_results_file" | jq '[.components[].warnings | length] | add')
-num_error_components=$(cat "$ec_results_file" | jq '[.components[] | select(.violations) | .name] | length')
-num_warning_components=$(cat "$ec_results_file" | jq '[.components[] | select(.warnings) | .name] | length')
+  MESSAGE="EC validation test $ec_test for $APPLICATION (<$WEB_URL/pipelineruns/$PIPELINE_NAME|$PIPELINE_NAME>) had $num_errors errors across $num_error_components components and $num_warnings warnings across $num_warning_components components"
 
-MESSAGE="EC validation test $ec_test for $APPLICATION (<$WEB_URL/pipelineruns/$PIPELINE_NAME|$PIPELINE_NAME>) had $num_errors errors across $num_error_components components and $num_warnings warnings across $num_warning_components components"
+  echo $MESSAGE
 
-echo $MESSAGE
-
-echo "sending slack message with file attachment"
-bash ../send-slack-message/send-slack-message.sh -v -c "$SLACK_CHANNEL" -m "$MESSAGE" -f "./ec-results-slack.yaml" 
-
+  echo "sending slack message with file attachment"
+  bash ../send-slack-message/send-slack-message.sh -v -c "$SLACK_CHANNEL" -m "$MESSAGE" -f "./$MODE-ec-results-slack.yaml" 
+done
