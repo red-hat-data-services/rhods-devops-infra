2205da95a5cdca7477f158a1db6af25188031bec
Mohammadi Iram <89964724+MohammadiIram@users.noreply.github.com>
2025-03-05 13:03:41 +0530

    Update ubuntu-install.sh

--- tools/snapshot-generator/ubuntu-install.sh
+++ tools/snapshot-generator/ubuntu-install.sh
@@ -10,7 +10,10 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-relea
 # Add the Kubernetes repository
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo tee /etc/apt/keyrings/kubernetes-archive-keyring.gpg > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /" \
+  | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Update package list again after adding the repo
 sudo apt-get update -y
